313f92d904415595adb5995b0a4ec77f3654e2e1
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-03-29 17:25:07 +0500

    implementing manual update for total cases

--- app/src/main/java/com/example/covid_19status/HomeFragment.kt
+++ app/src/main/java/com/example/covid_19status/HomeFragment.kt
@@ -9,11 +9,16 @@ import com.example.covid_19status.Constants.Companion.COIVD19_APP_ID
 import com.example.covid_19status.Constants.Companion.LOG_TAG
 import com.example.covid_19status.databinding.FragmentHomeBinding
 import com.example.covid_19status.response.SummaryResponse
+import org.json.JSONException
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.text.DecimalFormat
 
 /**
  * A simple [Fragment] subclass.
@@ -27,7 +32,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
-        // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         return binding.root
     }
@@ -35,72 +39,53 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getData()
+        val formatter = DecimalFormat()
 
-        binding.refresh.setOnRefreshListener {
-            binding.updatingLabel.visibility = View.VISIBLE
-            binding.progressBar.visibility = View.VISIBLE
-            getData()
-            binding.refresh.isRefreshing = false
+        try {
+            val obj = JSONObject(loadJSONFromAssets()!!)
+            val jsonArray = obj.getJSONArray("Data")
+
+            var totalConfirmed = 0
+            var totalDeaths = 0
+            var totalRecovered = 0
+            var totalActive = 0
+
+            for (i in 0 until jsonArray.length()) {
+                val inside = jsonArray.getJSONObject(i)
+                val confirmed = inside.get("Confirmed") as Int
+                val deaths = inside.get("Deaths") as Int
+                val recovered = inside.get("Recovered") as Int
+                val active = inside.get("Active") as Int
+
+                totalConfirmed += confirmed
+                totalDeaths += deaths
+                totalRecovered += recovered
+                totalActive += active
+            }
+            this.binding.totalCases.text = formatter.format(totalConfirmed)
+            this.binding.totalDeaths.text = formatter.format(totalDeaths)
+            this.binding.totalRecovered.text = formatter.format(totalRecovered)
+            this.binding.totalActive.text = formatter.format(totalActive)
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
+
     }
 
-    private fun getData() {
-        commit(object : HomeRetrofitResponseListener {
-            override fun onSuccess(
-                call: Call<SummaryResponse>,
-                response: Response<SummaryResponse>
-            ) {
-                binding.updatingLabel.visibility = View.INVISIBLE
-                binding.progressBar.visibility = View.INVISIBLE
-            }
-
-            override fun onFailure(call: Call<SummaryResponse>, t: Throwable) {
-                Log.d(LOG_TAG, t.localizedMessage, t)
-            }
-
-        })
-    }
-
-    private fun commit(listener: HomeRetrofitResponseListener) {
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BaseURL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service = retrofit.create(APIService::class.java)
-        val call = service.getSummaryData(COIVD19_APP_ID)
-        call.enqueue(object : Callback<SummaryResponse> {
-            override fun onFailure(call: Call<SummaryResponse>, t: Throwable) {
-                listener.onFailure(call, t)
-            }
-
-            override fun onResponse(
-                call: Call<SummaryResponse>,
-                response: Response<SummaryResponse>
-            ) {
-                val data = response.body()
-                var recovered = 0
-                var deaths = 0
-                var cases = 0
-                data?.countries?.forEach { country ->
-                    recovered += country.totalRecovered ?: 0
-                    deaths += country.totalDeaths ?: 0
-                    cases += country.totalConfirmed ?: 0
-                }
-                binding.totalCases.text = cases.toString()
-                binding.totalDeaths.text = deaths.toString()
-                binding.totalRecovered.text = recovered.toString()
-                listener.onSuccess(call, response)
-            }
-        })
-    }
-
-    interface HomeRetrofitResponseListener {
-
-        fun onSuccess(call: Call<SummaryResponse>, response: Response<SummaryResponse>)
-        fun onFailure(call: Call<SummaryResponse>, t: Throwable)
+    private fun loadJSONFromAssets(): String? {
+        val json: String?
+        try {
+            val file = requireActivity().assets.open("coid_19_latest.json")
+            val size = file.available()
+            val buffer = ByteArray(size)
+            file.read(buffer)
+            file.close()
+            json = String(buffer, StandardCharsets.UTF_8)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return json
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -108,15 +93,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_refresh -> {
-                binding.updatingLabel.visibility = View.VISIBLE
-                binding.progressBar.visibility = View.VISIBLE
-                getData()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
